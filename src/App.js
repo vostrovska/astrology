37732f6275df70834261c99f1b8b46bceaf81d46
vostrovska <v7753762909@gmail.com>
2022-09-29 08:34:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { data } from './data'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [sign, setSign] = useState(data[0])
+  const [showText, setShowText] = useState(false)
+const showTextClick = (item) =>{
+  item.showMore = !item.showMore
+  setShowText(!showText)
+}
+const{imageOne, imageTwo,imageThree, descriptionOne, descriptionTwo, descriptionThree} = data[sign]
+const nextPicture =() => {
+  setSign((sign => {
+    sign++;
+    return sign
+  }))
+}
+const prevPicture =() => {
+  setSign((sign => {
+    sign++;
+    return sign
+  }))
+}
+
+  return (<div>
+    <div className='container'>
+      <h1>Elements of Sign of Zodiak</h1>
+    </div>
+    <div>
+      {sign.map((item =>{
+        const { id, name, description, image,showMore} = item;
+        return(<div>
+          <div className='container'> 
+            <h2>{id} - {name}</h2>
+          </div>
+          <div className='container'>
+            <p>{showMore ? description : description.substring (0,200) + "..."}
+            <button onClick={() => showTextClick(item)}>{showMore ? "Show less" : "Show more"}</button>
+            </p>
+          </div>
+          <div className='container'>
+            <img src={image} width ="500px" alt="zodiak"/>
+          </div>
+           <div className='container'>
+            <button onClick={prevPicture}>Previos</button>
+            <button onClick={nextPicture}>Next</button>
+          </div> 
+        </div>
+
+        )
+      }))}
+    </div>
     </div>
   );
 }
